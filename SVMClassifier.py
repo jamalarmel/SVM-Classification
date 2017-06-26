b40ead9e0351243690255807e3232d0e05fb272b
Jamal Armel <jamalarmel@gmail.com>
2017-06-26 13:44:42 +0300

    added Kfold cross-validation

--- SVMClassifier.py
+++ SVMClassifier.py
@@ -1,117 +1,74 @@
-import csv
-import time
 import glob
-import numpy as np
+import numpy
+import pandas as pd
 from sklearn.preprocessing import LabelBinarizer
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.feature_extraction.text import (CountVectorizer, TfidfTransformer)
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import LinearSVC
-from sklearn.metrics import (confusion_matrix, classification_report)
+from sklearn.pipeline import Pipeline
+from sklearn.cross_validation import KFold
+from sklearn.metrics import (confusion_matrix, classification_report, accuracy_score)
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-class Doc_Classifier:
-    X_train=[]
-    X_test=[]
-    Y_train=[]
-    y=[]
-    Y1=[]
-    size=0
-    train_ex=0
+df = pd.read_csv("corpus.csv", sep=",", encoding="latin-1")
+
+df = df.set_index('id')
+df.columns = ['class', 'text']
+
+data = df.reindex(numpy.random.permutation(df.index))
 
 
-    def __init__(self):
-        #List to store input text
-        data_input=[]
-        #List to store output labels
-        data_output=[]
-        train_text=[]
-        
-        self.train_ex=int(raw_input('Enter the number of examples that should be used to train the model\n'))
 
-        with open('corpus.csv','r') as f:
-            train_csv=csv.reader(f)
-            
-            self.size=-1
-            for row in train_csv:
-                if self.size==-1:
-                    self.size=0
-                else:
-                    self.size=self.size+1
-                    data_input.append(row[0])
-                    data_output.append(row[1])
-        print 'There are ',self.size,' examples in the corpus\n'    
-          
-        #Generate a  permutation to re-shuffle the corpus so that training and testing data can be split randomly          
-        perm=np.random.permutation(self.size)
-        
-        #Shuffle the entire corpus            
-        for p in perm:
-            train_text.append(data_input[p])
-            self.Y_train.append(data_output[p])
-        
-        self.X_train = np.array(train_text[:self.train_ex])
-        self.X_test  = np.array(train_text[self.train_ex:self.size])
-        # files = glob.glob("corpus/*.txt")
-        # lines = []
-        # for fle in files:
-        #     with open(fle) as f:
-        #         lines += f.readlines()        
-        # self.X_test = np.array(lines)
-    	
+pipeline = Pipeline([
+    ('count_vectorizer',   CountVectorizer(ngram_range=(1, 2))),
+    ('tfidf',              TfidfTransformer()),
+    ('classifier',         OneVsRestClassifier(LinearSVC()))
+])
 
-        self.lb=LabelBinarizer()
-        self.Y1=self.Y_train[:self.train_ex]
-        self.y = self.lb.fit_transform(self.Y1)
-            
+k_fold = KFold(n=len(data), n_folds=6, shuffle=True)
 
-    def SVM_LinearSVC(self):        
-        SVM_Classifier = Pipeline([
-                ('vectorizer', CountVectorizer()),
-                ('tfidf', TfidfTransformer()),
-                ('clf', OneVsRestClassifier(LinearSVC()))
-                ])
-         
-        SVM_Classifier.fit(self.X_train,self.y)
-         
-        predicted = SVM_Classifier.predict(self.X_test)
-        y_pred = self.lb.inverse_transform(predicted)
-         
-        i=self.train_ex
-        correct=0
-        for label in y_pred:
-            if label==self.Y_train[i]:
-                correct=correct+1
-            i = i + 1
-        for item, labels in zip(self.X_test, y_pred):
-            print('Item: {0} => Label: {1}'.format(item, labels))
+for train_indices, test_indices in k_fold:
+    train_text = data.iloc[train_indices]['text'].values
+    train_y = data.iloc[train_indices]['class'].values.astype(str)
 
-        print 'Number of Examples used for Training',self.train_ex
-        print 'Number of Correctly classified',correct
-        print 'Total number of samples classified in Test data',self.size-self.train_ex
-        print 'The resulting accuracy using Linear SVC is ',(float(correct)*100/float(self.size-self.train_ex)),'%\n'
+    test_text = data.iloc[test_indices]['text'].values
+    test_y = data.iloc[test_indices]['class'].values.astype(str)
 
-        cm=confusion_matrix(self.Y_train[self.train_ex:self.size],y_pred)
-        percentage_matrix = 100 * cm / cm.sum(axis=1).astype(float)
-        plt.figure(figsize=(16, 16))
-        sns.heatmap(percentage_matrix, annot=True,  fmt='.2f', xticklabels=['Python', 'Java', 'Scala'], yticklabels=['Python', 'Java', 'Scala']);
-        plt.title('Confusion Matrix (Percentage)');
-        plt.show()
-        print(classification_report(self.Y_train[self.train_ex:self.size], y_pred,target_names=['Scala', 'Java', 'Python']))
-     
-        return y_pred
+    #Enter unseen data here
+    #files = glob.glob("corpus/*.txt")
+    #lines = []
+    #for fle in files:
+    #    with open(fle) as f:
+    #        lines += f.readlines()        
+    #test_text = numpy.array(lines)
+    #################################
+
+    lb = LabelBinarizer()
+    Z = lb.fit_transform(train_y)
+
+    pipeline.fit(train_text, Z)
+    predicted = pipeline.predict(test_text)
+    predictions = lb.inverse_transform(predicted)
     
 
-start=time.time()
-print 'Initializing....'
-n=Doc_Classifier()
-start=time.time()
+    df2=pd.DataFrame(predictions)
+    df2.index+=1
+    df2.index.name='Id'
+    df2.columns=['Label']
+    df2.to_csv('results.csv',header=True)
+    for item, labels in zip(test_text, predictions):
+        print('Item: {0} => Label: {1}'.format(item, labels))    
+   
 
-print '\nRunning SVM Classification'
-n.SVM_LinearSVC()
-time3=time.time()
-svm_time=time.time()-start
+    cm = confusion_matrix(test_y, predictions)
+    accuracy = accuracy_score(test_y, predictions)
 
+print 'The resulting accuracy using Linear SVC is ', (100 * accuracy), '%\n'
 
-print '\nThe running time was ',time.time()-start, ' seconds'
+percentage_matrix = 100 * cm / cm.sum(axis=1).astype(float)
+plt.figure(figsize=(16, 16))
+sns.heatmap(percentage_matrix, annot=True,  fmt='.2f', xticklabels=['Java', 'Python', 'Scala'], yticklabels=['Java', 'Python', 'Scala']);
+plt.title('Confusion Matrix (Percentage)');
+plt.show()
+print(classification_report(test_y, predictions,target_names=['Java', 'Python', 'Scala'], digits=2))
---
+++ SVMClassifierOld.py
@@ -0,0 +1,118 @@
+import csv
+import time
+import glob
+import numpy as np
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import LinearSVC
+from sklearn.metrics import (confusion_matrix, classification_report)
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class Doc_Classifier:
+    X_train=[]
+    X_test=[]
+    Y_train=[]
+    y=[]
+    Y1=[]
+    size=0
+    train_ex=0
+
+
+    def __init__(self):
+        #List to store input text
+        data_input=[]
+        #List to store output labels
+        data_output=[]
+        train_text=[]
+
+        self.train_ex=int(raw_input('Enter the number of examples that should be used to train the model\n'))
+
+        with open('corpus.csv','r') as f:
+            train_csv=csv.reader(f)
+            
+            self.size=-1
+            for row in train_csv:
+                if self.size==-1:
+                    self.size=0
+                else:
+                    self.size=self.size+1
+                    data_input.append(row[0])
+                    data_output.append(row[1])
+        print 'There are ',self.size,' examples in the corpus\n'    
+          
+        #Generate a  permutation to re-shuffle the corpus so that training and testing data can be split randomly          
+        perm=np.random.permutation(self.size)
+        
+        #Shuffle the entire corpus            
+        for p in perm:
+            train_text.append(data_input[p])
+            self.Y_train.append(data_output[p])
+        
+        self.X_train = np.array(train_text[:self.train_ex])
+        self.X_test  = np.array(train_text[self.train_ex:self.size])
+        # files = glob.glob("corpus/*.txt")
+        # lines = []
+        # for fle in files:
+        #     with open(fle) as f:
+        #         lines += f.readlines()        
+        # self.X_test = np.array(lines)
+    	
+
+        self.lb=LabelBinarizer()
+        self.Y1=self.Y_train[:self.train_ex]
+        self.y = self.lb.fit_transform(self.Y1)
+            
+
+    def SVM_LinearSVC(self):        
+        SVM_Classifier = Pipeline([
+                ('vectorizer', CountVectorizer()),
+                ('tfidf', TfidfTransformer()),
+                ('clf', OneVsRestClassifier(LinearSVC()))
+                ])
+         
+        SVM_Classifier.fit(self.X_train,self.y)
+         
+        predicted = SVM_Classifier.predict(self.X_test)
+        y_pred = self.lb.inverse_transform(predicted)
+         
+        i=self.train_ex
+        correct=0
+        for label in y_pred:
+            if label==self.Y_train[i]:
+                correct=correct+1
+            i = i + 1
+        for item, labels in zip(self.X_test, y_pred):
+            print('Item: {0} => Label: {1}'.format(item, labels))
+
+        print 'Number of Examples used for Training',self.train_ex
+        print 'Number of Correctly classified',correct
+        print 'Total number of samples classified in Test data',self.size-self.train_ex
+        print 'The resulting accuracy using Linear SVC is ',(float(correct)*100/float(self.size-self.train_ex)),'%\n'
+
+        cm=confusion_matrix(self.Y_train[self.train_ex:self.size],y_pred)
+        percentage_matrix = 100 * cm / cm.sum(axis=1).astype(float)
+        plt.figure(figsize=(16, 16))
+        sns.heatmap(percentage_matrix, annot=True,  fmt='.2f', xticklabels=['Python', 'Java', 'Scala'], yticklabels=['Python', 'Java', 'Scala']);
+        plt.title('Confusion Matrix (Percentage)');
+        plt.show()
+        print(classification_report(self.Y_train[self.train_ex:self.size], y_pred,target_names=['Scala', 'Java', 'Python']))
+
+        
+        return y_pred
+    
+
+start=time.time()
+print 'Initializing....'
+clf=Doc_Classifier()
+start=time.time()
+
+print '\nRunning SVM Classification'
+clf.SVM_LinearSVC()
+time3=time.time()
+svm_time=time.time()-start
+
+
+print '\nThe running time was ',time.time()-start, ' seconds'
